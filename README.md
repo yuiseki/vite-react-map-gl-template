b5d34cc3f5a3fafd1da11981f5adfd716f69394c
yuiseki <yuiseki@gmail.com>
2023-12-30 18:58:53 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Vite + react-map-gl
+
+## How to use
+
+```bash
+npx degit yuiseki/vite-react-map-gl-template#main my-react-map-gl-app
+cd my-react-map-gl-app
+npm install
+npm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import {
+  Map,
+  GeolocateControl,
+  NavigationControl,
+} from 'react-map-gl/maplibre';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Map
+        initialViewState={{
+          longitude: 0,
+          latitude: 0,
+          zoom: 6,
+        }}
+        hash={true}
+        style={{ width: '100%', height: '100%' }}
+        mapStyle="https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json"
+      >
+        <GeolocateControl position="top-right" />
+        <NavigationControl
+          position="top-right"
+          visualizePitch={true}
+          showZoom={true}
+          showCompass={true}
+        />
+      </Map>
+    </div>
+  );
+}
+
+export default App;
