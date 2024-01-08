3dfab539bad5d6121b91c30a28bdb030f5656223
yuiseki <yuiseki@gmail.com>
2024-01-08 12:52:32 +0900

    format

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import {
   Map,
   GeolocateControl,
   NavigationControl,
-} from 'react-map-gl/maplibre';
-import 'maplibre-gl/dist/maplibre-gl.css';
+} from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
 
 function App() {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div style={{ width: "100vw", height: "100vh" }}>
       <Map
         initialViewState={{
           longitude: 0,
@@ -15,7 +15,7 @@ function App() {
           zoom: 6,
         }}
         hash={true}
-        style={{ width: '100%', height: '100%' }}
+        style={{ width: "100%", height: "100%" }}
         mapStyle="https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json"
       >
         <GeolocateControl position="top-right" />
