66a3752c8fea4b32e4e8b07ec9ddcdcbdd3d38e1
yuiseki <yuiseki@gmail.com>
2025-11-07 16:52:10 +0900

    Select vector or raster tiles

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,18 @@ import {
   NavigationControl,
 } from "react-map-gl/maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
+import { useState } from "react";
+
+const mapStyles = {
+  osm_vector: "./styles/osm_vector.json",
+  osm_raster: "./styles/osm_raster.json",
+};
+
+type MapStyle = keyof typeof mapStyles;
 
 function App() {
+  const [mapStyle, setMapStyle] = useState<MapStyle>("osm_vector");
+
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <Map
@@ -16,9 +26,22 @@ function App() {
         }}
         hash={true}
         style={{ width: "100%", height: "100%" }}
-        //mapStyle="https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json"
-        mapStyle="./style.json"
+        mapStyle={mapStyles[mapStyle]}
       >
+        <div>
+          <select
+            value={mapStyle}
+            onChange={(e) => setMapStyle(e.target.value as MapStyle)}
+            style={{ position: "absolute", top: 10, left: 10, zIndex: 1 }}
+          >
+            <option value="osm_vector" selected={mapStyle === "osm_vector"}>
+              OSM Vector
+            </option>
+            <option value="osm_raster" selected={mapStyle === "osm_raster"}>
+              OSM Raster
+            </option>
+          </select>
+        </div>
         <GeolocateControl position="top-right" />
         <NavigationControl
           position="top-right"
